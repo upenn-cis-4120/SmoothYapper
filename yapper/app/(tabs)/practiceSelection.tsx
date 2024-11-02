9327a427563d0e5e4cd32abc944b0db12f5eb6eb
Haoze Wu <haozewu@seas.upenn.edu>
2024-11-02 19:03:41 -0400

    Add initial skeleton for the model selection

--- yapper/app/(tabs)/practiceSelection.tsx
+++ yapper/app/(tabs)/practiceSelection.tsx
@@ -1,10 +1,49 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { useEffect, useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import ModalSelection from "@/components/ModalSelection";
+
+const { ColorsPalette } = require("@/constants/colors.tsx");
 
 export default function practiceSelection() {
+    const [points, setPoints] = useState<number>(0);
+    const [selectedModal, setSelectedModal] = useState<{id: number; name: string; favoriteFood: string; age: number; avatar: any; scenario: string;} | undefined>(undefined);
+
+    useEffect(() => {
+        // Fetch data from the server
+        // fetch("https://api.example.com/points")
+        //     .then((response) => response.json())
+        //     .then((data) => {
+        //         setPoints(data);
+        //     });
+        setPoints(100);
+    }, []);
+
     return (
-        <View>
-            <Text>Practice Selection Screen</Text>
-            <Text style={{paddingTop: 150}}>Practice Selection Screen</Text>
+        <View style={styles.componentWrapper}>
+            <Text>Overall Poins {points}</Text>
+            <ModalSelection setSelectedModal={setSelectedModal}/>
+            <TouchableOpacity style={styles.playButton} onPress={
+                ()=> {
+                    console.log("Play button pressed");
+                    console.log(selectedModal);
+                }
+            }>
+                <MaterialIcons name="play-circle-outline" size={128} color={ColorsPalette.PrimaryColorLight}  />
+            </TouchableOpacity>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    componentWrapper: {
+        flex: 1,
+        alignItems: "center",
+        paddingTop: 80,
+        backgroundColor: ColorsPalette.FullWhite,
+    },
+    playButton: {
+        paddingTop: 40,
+    }
+});
---
+++ yapper/components/ModalSelection.tsx
@@ -0,0 +1,101 @@
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { useEffect, useState, Dispatch, SetStateAction } from "react";
+
+const { ColorsPalette } = require("@/constants/colors.tsx");
+
+type Props = {
+    setSelectedModal: Dispatch<SetStateAction<{ id: number; name: string; favoriteFood: string; age: number; avatar: any; scenario: string; } | undefined>>;
+}
+
+export default function ModalSelection({setSelectedModal} : Props) {
+    const [propertiesList, setPropertiesList] = useState<{ id: number; name: string; favoriteFood: string; age: number; avatar: any; scenario: string; }[]>([]);
+    const [index, setIndex] = useState<number>(0);
+
+    useEffect(() => {
+        // Fetch data from the server
+        // fetch("https://api.example.com/properties")
+        //     .then((response) => response.json())
+        //     .then((data) => {
+        //         setPropertiesList(data);
+        //     });
+        const monkResponse = [
+            {
+                id: 1,
+                name: "Ray Liu",
+                favoriteFood: "Sushi",
+                age: 21,
+                avatar: require('@/assets/images/cropped-tom.jpg'),
+                scenario: "Academic",
+            },
+            {
+                id: 2,
+                name: "Doraemon",
+                favoriteFood: "Dorayaki",
+                age: 1000000,
+                avatar: require('@/assets/images/cropped-doraemon.jpg'),
+                scenario: "Social",
+            },
+        ];
+        setPropertiesList(monkResponse);
+        setIndex(0);
+        setSelectedModal(monkResponse[0]);
+    }
+    , []);
+
+    useEffect(() => {
+        if (propertiesList.length > 0) {
+            setSelectedModal(propertiesList[index]);
+        }
+    }, [index, propertiesList, setSelectedModal]);
+
+    const handlePrevious = () => {
+        setIndex((prevIndex) => (prevIndex - 1 + propertiesList.length) % propertiesList.length);
+    };
+
+    const handleNext = () => {
+        setIndex((prevIndex) => (prevIndex + 1) % propertiesList.length);
+    };
+
+    return (
+        <View style={styles.componentWrapper}>
+            {propertiesList.length > 0 && index < propertiesList.length && (
+                <>
+                <View style={styles.selectionContainer}>
+                    <TouchableOpacity onPress={handlePrevious}>
+                        <Text>Previous</Text>
+                    </TouchableOpacity>
+                    <Image source={propertiesList[index].avatar} style={styles.avatar}/>
+                    <TouchableOpacity onPress={handleNext}>
+                        <Text>Next</Text>
+                    </TouchableOpacity>
+                </View>
+                    <Text>{propertiesList[index].name}</Text>
+                    <Text>Favorite Food: {propertiesList[index].favoriteFood}</Text>
+                    <Text>Age: {propertiesList[index].age}</Text>
+                    <Text>Scenario: {propertiesList[index].scenario}</Text>
+                </>
+            )}
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    componentWrapper: {
+        alignItems: "center",
+    },
+    selectionContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
+        padding: 20,
+    },
+    avatar: {
+        width: 256,
+        height: 256,
+        borderRadius: 128,
+        borderColor: ColorsPalette.SecondaryColorDeep,
+        borderWidth: 5,
+    },
+});
